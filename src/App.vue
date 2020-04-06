3e6ee3000eec388b73a895b4aabd43b670b0e3a3
tomekr <tomekr@gmail.com>
2020-04-06 13:48:38 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,78 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <table border='1' width='100%' style='border-collapse: collapse;'>
+       <tr>
+         <th>First Name</th>
+         <th>Email</th>
+         <th>City</th>
+         <th>Street</th>
+         <th>Actions</th>
+       </tr>
+    
+       <tr v-for='user in users' :key="user.id">
+         <td>{{ user.name }}</td>
+         <td>{{ user.email }}</td>
+         <td>{{ user.address.city }}</td>
+          <td>{{ user.address.street }}</td>
+         
+         <td>
+          <input type="button" @click="selectContact(user)" value="Select">
+          <input type="button" @click="deleteContact(contact.id)" value="Delete">
+         </td> 
+       </tr>
+     </table>
+     <br>
+        <form>
+          <label>First Name</label>
+          <input type="text" name="firstName" v-model="firstName">
+          <br>
+    
+          <label>Last Name</label>
+          <input type="text" name="lastName" v-model="lastName">
+          <br>
+    
+          <label>Email</label>
+          <input type="email" name="email" v-model="email">
+          <br>
+          
+          <input v-if="!id" type="button" @click="createContact(firstName, lastName, email)" value="Add">
+          <input v-if="id" type="button" @click="updateContact(id, firstName, lastName, email)" value="Update">
+          <input type="button" @click="clearForm()" value="Clear">
+          
+        </form>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import gql from 'graphql-tag'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  data(){
+        return {
+          id: null,
+          name: '',
+          email: '',
+          city:'',
+          street:''
+          }
+      },
+      apollo: {
+        users: gql`query {
+          users {
+            name,
+            email,
+            address{
+              city,
+              street
+            }
+          }
+        }`,
+      },
+ 
 }
+
 </script>
 
 <style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,26 @@
 import Vue from 'vue'
 import App from './App.vue'
+import ApolloClient from "apollo-boost"
+import VueApollo from "vue-apollo"
+require('dotenv').config()
 
 Vue.config.productionTip = false
 
+const apolloClient = new ApolloClient({
+  uri: "http://192.168.1.5:4000/graphql"
+  // process.env.URI
+  
+  
+})
+
+Vue.use(VueApollo)
+    const apolloProvider = new VueApollo({
+      defaultClient: apolloClient,
+    })
+
+
 new Vue({
   render: h => h(App),
+  apolloProvider,
+  
 }).$mount('#app')
